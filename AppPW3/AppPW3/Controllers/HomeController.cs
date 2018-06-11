7b9836107477d8d4b23dd0e0f2459a48290f37ff
nachocastineira <nacho2.0804@gmail.com>
2018-06-11 01:15:29 -0300

    vistas tareas y carpetas traen datos de BD

--- AppPW3/AppPW3/Controllers/HomeController.cs
+++ AppPW3/AppPW3/Controllers/HomeController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppPW3.Entidades;
+using AppPW3.Servicios;
 
 namespace AppPW3.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        UsuarioServices usuarioServices = new UsuarioServices();
+
+
         public ActionResult Index()
         {
             return View();
@@ -19,6 +23,22 @@ namespace AppPW3.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult VerificarLogin(Usuario usuario)
+        {
+            bool loginCorrecto = usuarioServices.VerificarLogin(usuario);
+
+            if (loginCorrecto) //si el verificar login es true lo redirige a su index, sino lo lleva al login de nuevo
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
         public ActionResult Logout()
         {
             return View();
@@ -27,6 +47,16 @@ namespace AppPW3.Controllers
         public ActionResult Registracion()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registracion(Usuario usuario)
+        {
+            //hay que terminarlo, y agregar mas condiciones por si el usuario ya existe que lo redirija a otro lado
+
+            usuarioServices.RegistrarUsuario(usuario);
+
+            return RedirectToAction("Login");
         }
     }
 }
