e6108f0657e8ff10d1993e8b960d4b8290d3c352
nachocastineira <nacho2.0804@gmail.com>
2018-06-20 20:12:47 -0300

    view detalle a medio hacer

--- AppPW3/AppPW3/Content/js/app.js
+++ AppPW3/AppPW3/Content/js/app.js
@@ -4,3 +4,14 @@ $("#menu-toggle").click(function (e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$(function () {
+    $('#datetimepicker5').datetimepicker({
+        defaultDate: "11/1/2013",
+        disabledDates: [
+            moment("12/25/2013"),
+            new Date(2013, 11 - 1, 21),
+            "11/22/2013 00:53"
+        ]
+    });
+});
