dc32c3a2ef087d2b33a2500ae7e22cef5a5b862c
jerotrica <jerotrica@gmail.com>
2018-07-01 02:59:57 -0300

    Revert "Merge branch 'master' of https://github.com/nachocastineira/PW3-TP"
    This reverts commit f87a2704cb4920bdeb05427a9393f924d7670591, reversing
    changes made to 127b442478c1b51f28b7a05b0f79be22983177ad.

--- AppPW3/AppPW3/Controllers/HomeController.cs
+++ AppPW3/AppPW3/Controllers/HomeController.cs
@@ -34,10 +34,23 @@ namespace AppPW3.Controllers
 
         public ActionResult Login()
         {
-
             return View();
         }
 
+        /*[HttpPost]
+        public ActionResult Login(Usuario usuario)
+        {
+            bool loginCorrecto = usuarioServices.VerificarLogin(usuario);
+
+            if (loginCorrecto) //si el verificar login es true lo redirige a su index, sino lo lleva al login de nuevo
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("IndexAlternativo");
+            }
+        }*/
         [HttpPost]
         public ActionResult Login(Usuario usuario)
         {
@@ -89,7 +102,7 @@ namespace AppPW3.Controllers
                             {
                                 usuarioServices.ModificarUsuarioActivo(usuario);
                                 Login(usuario);
-                                return RedirectToAction("index", "home");
+                                return RedirectToAction("index");
                             }
                             else
                             {   //si el mail ya existe, vuelve a la vista y se muestra error
